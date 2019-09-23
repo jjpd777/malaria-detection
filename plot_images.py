152814f705ec9511d72fbdd07a85e6e0ac09933c
juan <jpalacio@axletravel.com>
2019-09-23 02:12:11 -0500

    Plotting images for README

---
+++ plot_images.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import cv2
+from pathlib import Path
+import glob
+
+malaria = Path("./cell_images/malaria")
+non_malaria = Path("./cell_images/non-malaria")
+malaria = malaria.glob("*.png")
+non_malaria = non_malaria.glob("*.png")
+samples = list(malaria) + list(non_malaria)
+print(samples)
+f, ax = plt.subplots(2,5, figsize=(30,10))
+for i in range(10):
+    img = cv2.imread(str(samples[i]))
+    ax[i//5, i%5].imshow(img)
+    if i<5:
+        ax[i//5, i%5].set_title("Malaria")
+    else:
+        ax[i//5, i%5].set_title("Normal")
+    ax[i//5, i%5].axis()
+    ax[i//5, i%5].set_aspect('auto')
+plt.savefig("images.png")
