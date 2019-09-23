c65833203d2c8c41fdfcff7d812387db5ee5752d
juan <jpalacio@axletravel.com>
2019-09-23 02:26:00 -0500

    Re-arranged axis

--- plot_images.py
+++ plot_images.py
@@ -8,7 +8,15 @@ non_malaria = Path("./cell_images/non-malaria")
 malaria = malaria.glob("*.png")
 non_malaria = non_malaria.glob("*.png")
 samples = list(malaria) + list(non_malaria)
-print(samples)
+
+
+SMALL_SIZE = 12
+BIG_SIZE = 20
+
+plt.rc('axes', titlesize=BIG_SIZE)     # fontsize of the axes title
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+
 f, ax = plt.subplots(2,5, figsize=(30,10))
 for i in range(10):
     img = cv2.imread(str(samples[i]))
@@ -19,4 +27,4 @@ for i in range(10):
         ax[i//5, i%5].set_title("Normal")
     ax[i//5, i%5].axis()
     ax[i//5, i%5].set_aspect('auto')
-plt.savefig("images.png")
+plt.savefig("plotted_images.png")
